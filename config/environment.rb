18e19e926a2c856f88b52f34f39bb88ecf605be6
Siddharth Ram <siddharthram@gmail.com>
2013-02-02 08:57:46 -0800

    added config for heroku to environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,9 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Ximly::Application.initialize!
 
+#require Rails.root.join("config/initializers/settings") #this is not loaded automatically in the assets:precompile task, that's why we need this
 
-#config.action_mailer.default_url_options = { :host => 'localhost' }
+Ximly::Application.configure do
+	config.assets.initialize_on_precompile = false
+	config.action_mailer.default_url_options = { :host => 'http://mysterious-retreat-8129.herokuapp.com' }
+end
