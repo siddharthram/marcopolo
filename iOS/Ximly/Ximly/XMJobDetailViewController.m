850576d24fc08c6e15e234977eebeafa70b36c35
Sid Gidwani <sgidwani@gmail.com>
2013-02-15 02:57:20 -0800

    use job's imageData for image sent to evernote

--- iOS/Ximly/Ximly/XMJobDetailViewController.m
+++ iOS/Ximly/Ximly/XMJobDetailViewController.m
@@ -145,10 +145,9 @@
             
             NSString *titleText = self.job.title ? self.job.title : @"Untitled";
             
-            NSData *imageData = UIImagePNGRepresentation(self.job.image);
-            NSData *dataHash = [imageData md5];
-            EDAMData *edamData = [[EDAMData alloc] initWithBodyHash:dataHash size:imageData.length body:imageData];
-            EDAMResource* resource = [[EDAMResource alloc] initWithGuid:nil noteGuid:nil data:edamData mime:@"image/png" width:0 height:0 duration:0 active:0 recognition:0 attributes:nil updateSequenceNum:0 alternateData:nil];
+            NSData *dataHash = [self.job.imageData md5];
+            EDAMData *edamData = [[EDAMData alloc] initWithBodyHash:dataHash size:self.job.imageData.length body:self.job.imageData];
+            EDAMResource* resource = [[EDAMResource alloc] initWithGuid:nil noteGuid:nil data:edamData mime:@"image/jpeg" width:0 height:0 duration:0 active:0 recognition:0 attributes:nil updateSequenceNum:0 alternateData:nil];
             NSString *noteContent = [NSString stringWithFormat:@"<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
                                      "<!DOCTYPE en-note SYSTEM \"http://xml.evernote.com/pub/enml2.dtd\">"
                                      "<en-note>"
@@ -159,7 +158,7 @@
                                      "<br />"
                                      "<br />"
                                      "%@"
-                                     "</en-note>",titleText, transcribedText, [ENMLUtility mediaTagWithDataHash:dataHash mime:@"image/png"]];
+                                     "</en-note>",titleText, transcribedText, [ENMLUtility mediaTagWithDataHash:dataHash mime:@"image/jpeg"]];
             NSMutableArray* resources = [NSMutableArray arrayWithArray:@[resource]];
             EDAMNote *newNote = [[EDAMNote alloc] initWithGuid:nil title:titleText content:noteContent contentHash:nil contentLength:noteContent.length created:0 updated:0 deleted:0 active:YES updateSequenceNum:0 notebookGuid:nil tagGuids:nil resources:resources attributes:nil tagNames:nil];
             [[EvernoteNoteStore noteStore] createNote:newNote success:^(EDAMNote *note) {
@@ -170,7 +169,7 @@
             } failure:^(NSError *error) {
                 [self hideSendingToEvernoteUI];
                 NSLog(@"Error creating note : %@",error);
-                UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Error" message:[NSString stringWithFormat:@"Sorry, we were unable to send your note to Evernote. Error: %@", [error localizedDescription]] delegate:self cancelButtonTitle:@"Ok" otherButtonTitles:nil];
+                UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Error" message:[NSString stringWithFormat:@"Sorry, we were unable to send your note to Evernote. Error: %@", error] delegate:self cancelButtonTitle:@"Ok" otherButtonTitles:nil];
                 [alertView show];
             }];
         } 
